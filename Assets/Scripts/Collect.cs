3455b947129a793cde92b6a5ed7d1e4a7a05e130
Ayşe Yılmaz <ayseeyyilmaz96@gmail.com>
2023-04-16 14:29:14 +0300

    updtae

--- Assets/Scripts/Collect.cs
+++ Assets/Scripts/Collect.cs
@@ -4,25 +4,5 @@ using UnityEngine;
 
 public class Collect : MonoBehaviour
 {
-    public GameObject[] items;
-
-    public float secondSpawn = 0.5f;
-    public float min;
-    public float max;
-
-    private void Start()
-    {
-        
-    }
-
-    IEnumerator Spawn()
-    {
-        var wanted = Random.Range(min, max);
-        var position = new Vector3(wanted, transform.position.y);
-        GameObject gameObject = Instantiate(items[Random.Range(0, items.Length)],
-        position, Quaternion.identity);
-        yield return new WaitForSeconds(secondSpawn);
-        Destroy(gameObject, 5f);
-
-    }
+ 
 }
